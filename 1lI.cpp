b9c6ad58328745daa1640e991dca7faf10c0d450
Денис Пьянков <91528031+DenisPyankov@users.noreply.github.com>
2022-03-19 05:50:12 +0500

    Add files via upload

---
+++ 1lI.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <fstream>
+#include <list>
+using namespace std;
+
+int main()
+{
+	ifstream in("1.txt");
+	list <int> l={};
+	int s = 0;
+	double k = 0;
+	while (!in.eof()) {
+		int n;
+		in >> n;
+		k++;
+		s += n;
+		l.push_back(n);
+	}
+	l.sort();
+	for (int i : l)
+		cout << i << ", ";
+	cout << endl;
+	cout << s / k;
+}
---
+++ 2lI.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <list>
+using namespace std;
+
+int main()
+{
+	list <int> l;
+	double s = 0;
+	for (int i = 1; i <= 10000; i++) {
+		int n = rand() % 201 - 100;
+		s += n;
+		l.push_back(n);
+	};
+	for (int i : l)
+		cout << i << ", ";
+	cout << endl;
+	cout << s / 10000;
+}
